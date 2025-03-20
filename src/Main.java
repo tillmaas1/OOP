b3302de74374b6aaf6f9277212d60bd111e6e0f1
tillmaas1 <till.maas@gmx.de>
2025-03-20 11:28:25 +0100

    Aufgabenblatte 1: Aufgabe 1 und 2

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Rechteck r=new Rechteck(1,2,2,2);
+        Quadrat q=new Quadrat(1,3,2);
+        Kreis k=new Kreis(1,4,2,2);
+        r.Ausgabe();
+        System.out.println("\n");
+        q.Ausgabe();
+        System.out.println("\n");
+        k.Ausgabe();
+    }
+}
